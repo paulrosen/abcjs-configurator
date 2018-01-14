c2102332ce3ea4933f09b2655a75953b26c8f27c
Paul Rosen <paul@paulrosen.net>
2018-01-14 13:23:49 -0500

    Fix linting and editor configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,21 @@
+// https://eslint.org/docs/user-guide/configuring
+
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // add your custom rules here
-  rules: {
-    'space-before-function-paren': [
-      2,
-      {
-        anonymous: 'always',
-        named: 'never'
-      }
-    ],
-  },
-  globals: {
-  }
+	root: true,
+	parser: 'babel-eslint',
+	parserOptions: {
+		sourceType: 'module'
+	},
+	env: {
+		browser: true,
+	},
+	// required to lint *.vue files
+	plugins: [
+		'html'
+	],
+	// add your custom rules here
+	'rules': {
+		// allow debugger during development
+		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+	}
 }
