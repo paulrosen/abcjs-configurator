8e3839b61f3048f1245afad36301b1ccbdb5cb22
Paul Rosen <paul@paulrosen.net>
2018-02-05 11:34:20 -0500

    Put in, and comment out, the vuetify a-la-carte. (Doesn't save much.)

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -2,6 +2,52 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import colors from 'vuetify/es5/util/colors'
 
+// import Vuetify from 'vuetify/es5/components/Vuetify'
+// import VApp from 'vuetify/es5/components/VApp'
+// import VNavigationDrawer from 'vuetify/es5/components/VNavigationDrawer'
+// import VFooter from 'vuetify/es5/components/VFooter'
+// import VList from 'vuetify/es5/components/VList'
+// import VBtn from 'vuetify/es5/components/VBtn'
+// import VIcon from 'vuetify/es5/components/VIcon'
+// import VGrid from 'vuetify/es5/components/VGrid'
+// import VToolbar from 'vuetify/es5/components/VToolbar'
+// import VCard from 'vuetify/es5/components/VCard'
+// import VTextField from 'vuetify/es5/components/VTextField'
+// import VDialog from 'vuetify/es5/components/VDialog'
+// import VCheckbox from 'vuetify/es5/components/VCheckbox'
+// import VSelect from 'vuetify/es5/components/VSelect'
+// import VBtnToggle from 'vuetify/es5/components/VBtnToggle'
+// import transitions from 'vuetify/es5/components/transitions'
+
+// Vue.use(Vuetify, {
+// 	components: {
+// 		VApp,
+// 		VNavigationDrawer,
+// 		VFooter,
+// 		VList,
+// 		VBtn,
+// 		VIcon,
+// 		VGrid,
+// 		VToolbar,
+// 		VCard,
+// 		VTextField,
+// 		VDialog,
+// 		VCheckbox,
+// 		VSelect,
+// 		VBtnToggle,
+// 		transitions
+// 	},
+// 	theme: {
+// 		primary: '#121212', // a color that is not in the material colors palette
+// 		accent: colors.grey.darken3,
+// 		secondary: colors.amber.darken3,
+// 		info: colors.teal.lighten1,
+// 		warning: colors.amber.base,
+// 		error: colors.deepOrange.accent4,
+// 		success: colors.green.accent3
+// 	}
+// })
+
 Vue.use(Vuetify, {
   theme: {
     primary: '#121212', // a color that is not in the material colors palette
